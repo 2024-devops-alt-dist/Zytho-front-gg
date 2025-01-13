c356079051051cd3dd8b2fdaad5f3c9cf7caf8d3
Gwen <gwendoline.garlet@live.fr>
2025-01-13 10:42:37 +0100

    Init: Mise en place de react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import {} from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import ListBeers from "./pages/beer/ListBeers";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-600">Hello World</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/beers" element={<ListBeers />} />
+      </Routes>
+    </Router>
   );
 }
 
