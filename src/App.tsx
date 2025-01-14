3d013605d8f706eec9b13f4207a8c92341f82a63
Gwen <gwendoline.garlet@live.fr>
2025-01-14 15:11:43 +0100

    fix: Ajout des routes brasserie

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,19 @@ import { PrimeReactProvider } from "primereact/api";
 import DetailBeer from "./pages/beer/DetailBeer";
 import "primereact/resources/themes/saga-blue/theme.css"; // Remplacez par votre thème
 import "primereact/resources/primereact.min.css";
+import ListBreweries from "./pages/brewerie/ListBreweries";
+import NavBar from "./components/NavBar";
+import DetailBrewerie from "./pages/brewerie/DetailBrewerie";
 
 function App() {
   return (
     <PrimeReactProvider>
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/breweries" element={<ListBreweries />} />
+          <Route path="/breweries/:id" element={<DetailBrewerie />} />
           <Route path="/beers" element={<ListBeers />} />
           <Route path="/beers/:id" element={<DetailBeer />} />
         </Routes>
