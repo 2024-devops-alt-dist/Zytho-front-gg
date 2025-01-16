b28dd55551637777e0a8f0dbf3f9af8931add722
Gwen <gwendoline.garlet@live.fr>
2025-01-16 15:37:47 +0100

    fix: test

--- src/services/BeerService.ts
+++ src/services/BeerService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import BeerInterface from "../entity/BeerInterface";
 
 class BeerService {
-  public async findAllBeers(): Promise<BeerInterface[]> {
+  public async findAll(): Promise<BeerInterface[]> {
     try {
       const resp = await axios.get("/api/beers");
       return resp.data;
@@ -11,18 +11,16 @@ class BeerService {
     }
   }
 
-  public async findByIdBeer(idBeer: number): Promise<BeerInterface> {
+  public async findById(idBeer: number): Promise<BeerInterface> {
     try {
       const resp = await axios.get("/api/beers/" + idBeer);
-      console.log(resp.data);
-
       return resp.data;
     } catch {
       throw new Error("Erreur lors de la récupération des bières.");
     }
   }
 
-  public async updateBeer(
+  public async update(
     idBeer: number,
     updateBeer: Partial<BeerInterface>
   ): Promise<BeerInterface> {
@@ -31,6 +29,26 @@ class BeerService {
       return resp.data;
     } catch {
       throw new Error("Erreur lors de la mise à jour de la bière.");
+    }
+  }
+  public async create(newBeer: BeerInterface): Promise<BeerInterface> {
+    console.log("Envoi de la requête à /api/beers avec le payload :", newBeer);
+    try {
+      const resp = await axios.post("/api/beers/", newBeer);
+      return resp.data;
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("Erreur lors de la création de la bière.");
+    }
+  }
+
+  public async deleteById(idBeer: number): Promise<BeerInterface> {
+    try {
+      const resp = await axios.delete("/api/beers/" + idBeer);
+      return resp.data;
+    } catch {
+      throw new Error("Erreur lors de la récupération des bières.");
     }
   }
 }
