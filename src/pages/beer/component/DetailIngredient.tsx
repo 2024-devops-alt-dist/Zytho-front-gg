383d6ae046a2bd89c369a7846aa96e76fc1a17b9
Gwen <gwendoline.garlet@live.fr>
2025-02-08 16:38:12 +0100

    Refacto code sur DetailIngredient

--- src/pages/beer/component/DetailIngredient.tsx
+++ src/pages/beer/component/DetailIngredient.tsx
@@ -3,33 +3,32 @@
 import { useEffect, useState } from "react";
 import IngredientService from "../../../services/IngredientService";
 import { IngredientInterface } from "../../../entity/IngredientInterface";
+import { Card } from "primereact/card";
+// import { IngredientInterface } from "../../../entity/IngredientInterface";
 
 interface DetailIngredientProps {
   idBeer: number;
 }
 
 const DetailIngredient = ({ idBeer }: DetailIngredientProps) => {
-  const [ingredient, setIngredient] = useState<IngredientInterface[]>();
+  const [ingredients, setIngredient] = useState<IngredientInterface[]>();
   const [ingredientsService] = useState(new IngredientService());
+  const fetchIngredients = async () => {
+    const data = await ingredientsService.findByBeerIngerdient(Number(idBeer));
+
+    setIngredient(data);
+  };
   useEffect(() => {
-    const fetchBreweries = async () => {
-      try {
-        ingredientsService.findByBeerIngerdient(Number(idBeer)).then((data) => {
-          setIngredient(data);
-          console.log(ingredient);
-        });
-      } catch (error) {
-        console.error("Error fetching breweries:", error);
-      }
-    };
-    fetchBreweries();
-  }, [idBeer, ingredientsService]);
+    fetchIngredients();
+  }, [idBeer]);
 
   return (
-    ingredient &&
-    ingredient.map((i) => {
-      return <div key={i.id_ingredient}>{i.name}</div>;
-    })
+    <div className="flex justify-around w-full">
+      {ingredients &&
+        ingredients.map((i) => {
+          return <Card title={i.name} key={i.id_ingredient}></Card>;
+        })}
+    </div>
   );
 };
 export default DetailIngredient;
