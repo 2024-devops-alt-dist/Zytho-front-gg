f11c8f49d7e49737e013225e8e3f40444e46b28e
Gwen <gwendoline.garlet@live.fr>
2025-02-14 15:27:47 +0100

    TEST

--- src/entity/BeerInterface.ts
+++ src/entity/BeerInterface.ts
@@ -2,12 +2,14 @@ export default interface BeerInterface {
   id_beer: number;
   id_brewerie: number;
   id_category: number;
+  category_name: string;
   id_picture: number;
   name: string;
   abv: number;
   color: string;
   description: string;
   price: number;
+  category: string;
   type: "beer";
   created_at: Date;
   updated_at: Date;
